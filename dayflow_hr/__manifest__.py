b43fa80901dfd45da7a50d829c6a1de023e210e7
B Spoorthi <bspoorthi79@gmail.com>
2026-08-22 12:23:34 +0530

    feat: build HR payroll UI views and configure manifest

---
+++ dayflow_hr/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Dayflow HR',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'HR Payroll and Leave Management',
+    'depends': ['base'],
+    'data': [
+        'views/payroll_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
